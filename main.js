e3e6e1c76cc094177ce723455658b7f911662532
Shaw Summa <shawsumma@gmail.com>
2023-10-28 00:20:02 -0400

    global raylib
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ import raylib from 'raylib';
 import {start} from 'temper-raylib/start.js';
 
 start(raylib);
+
+import * as app from 'temper-raylib/app.js'
--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import os
 for ent in os.listdir('temper.out/py'):
     sys.path.append('temper.out/py/' + ent)
 
-from temper_raylib.start import start
-import temper_raylib.rl.types as types
+import temper_raylib.rl.raylib as traylib
 
 def make_iter(the_type):
     if not isinstance(the_type, type) or the_type in [str, int, bool, float]:
@@ -15,15 +14,20 @@ def make_iter(the_type):
     def inner(self):
         return iter(getattr(self, index) for index in the_type.__slots__)
     
-    the_type.__iter__ = inner
-
-pairs = vars(types)
+    try:
+        the_type.__iter__ = inner
+        return True
+    except Exception:
+        return False
+        
+pairs = vars(traylib)
 
 for key in pairs:
     make_iter(pairs[key])
 
 def main():
-    start(raylib)
+    traylib.use(raylib)
+    import temper_raylib.app
 
 if __name__ == '__main__':
     main()
