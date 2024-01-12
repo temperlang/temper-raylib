f8ec8816704640ea1d763ebed652e358a9bd0781
Shaw Summa <shawsumma@gmail.com>
2024-01-12 00:41:20 -0500

    Debug
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

---
+++ launch.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import raylib from 'raylib';
+
+import { use } from 'temper-raylib/rl/raylib.js'
+
+import { argv } from 'node:process';
+
+const game = argv[2];
+
+use(raylib);
+
+(async () => {
+    const { main } = await import(`temper-raylib/games/${game}.js`);
+    try {
+        main(argv.slice(3));
+    } catch (e) {
+        console.error(e);
+    }
+})();
